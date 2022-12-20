db89fd02823993393f4e190b8c1b29fbd6ae7a54
kevinMaga <kerimaga@espol.edu.ec>
2022-12-19 22:24:11 -0500

    clase1

---
+++ Galeria/src/main/java/com/mycompany/galeria/Album.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.galeria;
+
+/**
+ *
+ * @author Kevin Magallanes
+ */
+public class Album {
+    private String nombre;
+    private String descripcion;
+    private ArrayList<Foto> fotoContenidas;
+    
+    public Album(String nombre,String descripcion,ArrayList<Foto> fotoContenidas){
+        this.nombre=nombre;
+        this.descripcion=descripcion;
+        this.fotoContenidas = new ArrayList<>();
+    }
+    
+    public void añadirPersonas(){
+    
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public ArrayList<Foto> getFotoContenidas() {
+        return fotoContenidas;
+    }
+
+    public void setFotoContenidas(ArrayList<Foto> fotoContenidas) {
+        this.fotoContenidas = fotoContenidas;
+    }
+}
